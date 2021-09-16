1036292b11aab5d170edb9fcc3508eeaa8684bbc
ismael2307 <ismael_1312214239@uptecamac.edu.mx>
2021-09-15 21:18:08 -0500

    agregando

---
+++ main1.js
@@ -0,0 +1,75 @@
+//     &&//
+
+var a= 1;
+var b= 2;
+var c= 3;
+var d= 4;
+var e= 5;
+
+switch(a)
+{
+    case 1:
+        console.log("Este mes es septiembre");
+        break;
+    case 2:
+        console.log("Este mes es julio");
+        break;           
+}
+
+switch(b)
+{
+    case 1:
+        console.log("penal para rayados");
+        break;
+    case 2:
+        console.log("un clasico");
+        break;    
+}
+
+switch(c)
+{
+    case 1:
+        console.log("¿que clase es si favorita");
+        break;
+    case 2:
+        console.log("la clase de algoritmos mi favorita");
+        break;
+    case 3:
+        console.log("naaa nose crea");
+        break;        
+}
+
+switch(d)
+{
+    case 1:
+        console.log("el pan de muerto es el mejor");
+        break;
+    case 2:
+        console.log("a");
+        break;
+    case 3:
+        console.log("e");
+        break;
+    case 4:
+        console.log("con atole champurrado ");
+        break;            
+}
+
+switch(e)
+{
+    case 1:
+        console.log("hola");
+        break;
+    case 2:
+        console.log("mi nombre es roself ");
+        break;
+    case 3:
+        console.log("soy parte de la flota de explradores ");
+        break;
+    case 4:
+        console.log("desea que le ayude en algo");
+        break;
+    case 5:
+        console.log("señor");
+        break;                
+}
